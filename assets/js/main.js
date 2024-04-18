387686f6fe0df95dcb7a0351db2e89e4ed567f58
unknown <alaminbinmonsur@gmail.com>
2024-04-18 13:51:35 +0600

    modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,19 @@
 (function ($) {
     ("use strict");
-
      /*-- recent project slider --*/
-     var reviewSlider = $(".reviews-slider");
-     if (reviewSlider.length) {
-         $('.reviews-slider').slick({
-             slidesToShow: 2,
+     var collectionsSlider = $(".collections-slider");
+     if (collectionsSlider.length) {
+         $('.collections-slider').slick({
+             slidesToShow: 1,
              slidesToScroll: 1,
              infinite: true,
-             autoplay: true,
+             autoplay: false,
              arrows: true,
              fade: false,
              autoplaySpeed: 3000,
-             responsive: [
-                {
-                  breakpoint: 768,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                  }
-                },
-              ]
          });
      }
-     /*-- recent project slider --*/
-     var treatmentresultSlider = $(".treatment-result-slider");
-     if (treatmentresultSlider.length) {
-         $('.treatment-result-slider').slick({
-             slidesToShow: 2,
-             slidesToScroll: 1,
-             infinite: true,
-             autoplay: true,
-             arrows: true,
-             fade: false,
-             autoplaySpeed: 3000,
-             responsive: [
-                {
-                  breakpoint: 768,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                  }
-                },
-              ]
-         });
-     }
+    
 
    
 
